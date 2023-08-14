0c29e58476f30b8360af5c16946b5024b5dac4cc
Chen-Pei Chiang <132428265+benson8902@users.noreply.github.com>
2023-08-14 18:52:15 +0800

    Add files via upload

--- Code/Download_SRR/cellranger_count.sh
+++ Code/Download_SRR/cellranger_count.sh
@@ -2,14 +2,12 @@
 
 export PATH=/media/data/scRNA_analysis/cellranger-7.1.0:$PATH
 
-for ((i=28; i<=31; i++))
-do
-    SRR="SRR133764${i}"
-    if [ ! -d "${SRR}" ]; then
-            echo "Directory ${SRR} doesn't exist, skipping!"
-            continue
-    fi
+while read -r SRR; do
+	if [ ! -d "${SRR}" ]; then
+		echo "Directory ${SRR} doesn't exist, skipping!"
+		coutinue
+	fi
 
-    echo "Running cellranger count for ${SRR}"
-    nohup cellranger count --id=run_count_pbmc_${SRR} --fastqs=./pbmc --sample=${SRR} --transcriptome=/media/data/ref/cellranger/refdata-gex-GRCh38-2020-A
-done
+	echo "Running cellranger count for ${SRR}"
+	nohup cellranger count --id=run_count_pbmc_${SRR} --fastqs=./pbmc --sample=${SRR} --transcriptome=/media/data/ref/cellranger/refdata-gex-GRCh38-2020-A
+done < SRR_Acc_List.txt
