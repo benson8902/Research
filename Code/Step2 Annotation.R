e3353e174778d4b21a0c9101ca7d6690ac1287f0
Chen-Pei Chiang <132428265+benson8902@users.noreply.github.com>
2023-08-15 10:39:15 +0800

    Add files via upload

--- Code/Step2 Annotation.R
+++ Code/Step2 Annotation.R
@@ -9,13 +9,13 @@ library(pbmcref.SeuratData)
 getwd()
 
 # setting directory location
-setwd("C:\\Users\\User\\Desktop\\論文\\PBMC_Data\\HCA\\Immunophenotyping of COVID-19 and influenza highlights the role of type I interferons in development of severe COVID-19")
+setwd("C:\\Users\\User\\Desktop\\論文\\Research\\Code\\Data\\Annotation")
 
 # Convert anndata to h5seurat
-Convert("benson_pbmc_healthy_donor.h5ad", dest = "h5seurat", overwrite = T)
+Convert("C:\\Users\\User\\Desktop\\論文\\Research\\Code\\Data\\GSE165080_healthy.h5ad", dest = "h5seurat", overwrite = T)
 
 # Load h5seurat
-pbmc = LoadH5Seurat("benson_pbmc_healthy_donor.h5seurat", assays = "RNA", meta.data = FALSE, misc = FALSE)
+pbmc = LoadH5Seurat("C:\\Users\\User\\Desktop\\論文\\Research\\Code\\Data\\GSE165080_healthy.h5seurat", assays = "RNA", meta.data = FALSE, misc = FALSE)
 
 # Cell type annotation
 pbmc = RunAzimuth(pbmc, reference = "pbmcref")
@@ -24,8 +24,8 @@ pbmc = RunAzimuth(pbmc, reference = "pbmcref")
 pbmc = DietSeurat(pbmc)
 
 # Save and convert to h5ad
-SaveH5Seurat(pbmc, filename = "benson_pbmc_post_healthy_donor.h5seurat", overwrite = T)
-Convert("benson_pbmc_post_healthy_donor.h5seurat", dest = "h5ad", overwrite = T)
+SaveH5Seurat(pbmc, filename = "GSE165080_healthy_annotation.h5seurat", overwrite = T)
+Convert("GSE165080_healthy_annotation.h5seurat", dest = "h5ad", overwrite = T)
 
 
 
