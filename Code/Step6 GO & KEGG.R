4a5df96577d2149705d830344799935a9c84404a
Chen-Pei Chiang <132428265+benson8902@users.noreply.github.com>
2023-11-08 16:20:31 +0800

    Add files via upload

--- Code/Step6 GO & KEGG.R
+++ Code/Step6 GO & KEGG.R
@@ -14,7 +14,6 @@ library(ggplot2)
 library(R.utils)
 
 
-
 R.utils::setOption("clusterProfiler.download.method",'auto')
 createKEGGdb::create_kegg_db('hsa')
 
@@ -23,32 +22,39 @@ createKEGGdb::create_kegg_db('hsa')
 getwd()
 # setting directory location
 setwd("C:/Users/benson/Desktop/Research/")
-# 讀取.csv文件
-negative <- read.csv('negative_celltype_genes.csv', na.strings = c("None",""))
 
+# 讀取.csv文件
+negative <- read.csv('pearson_negative_celltype_genes_top200.csv', na.strings = c("None",""))
+# 讀取.csv文件
+positive <- read.csv('pearson_positive_celltype_genes_top200.csv', na.strings = c("None",""))
+
+
+
+
+
+# Negative
 # 提取cell type列表
-celltype <- negative$X
+celltype_N <- negative$X
 
 # 初始化一個空的列表來存儲結果
 genes <- list()
 
 # 對每個細胞類型進行操作
-genes <- lapply(celltype, function(x) {
-  temp <- negative[which(celltype == x), 2:ncol(negative[which(celltype == x),])]
+genes <- lapply(celltype_N, function(x) {
+  temp <- negative[which(celltype_N == x), 2:ncol(negative[which(celltype_N == x),])]
   temp <- temp %>% select_if(~!any(is.na(.))) # 刪除包含NA的列
   temp <- unlist(temp) # 將temp轉換為向量
   return(temp)})
 
 # 給列表的每個元素命名
-names(genes) <- celltype
+names(genes) <- celltype_N
 
 # 確認資料型態
 class(genes)
 
 # 對每種細胞類型的基因列表進行轉換
 ids <- lapply(genes, function(gene_list) {
-  bitr(gene_list, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
-})
+  bitr(gene_list, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)})
 
 # 進行GO(Gene Ontology) analysis
 go_results <- list()
@@ -68,25 +74,95 @@ for(i in names(ids)){
                                   organism     = 'hsa',
                                   pvalueCutoff = 0.05)}
 
-
-
 # 定義一個函數來繪製並保存結果
 plot_results <- function(results, result_type) {
-  lapply(names(results), function(celltype) {
+  lapply(names(results), function(celltype_N) {
     # 提取結果
-    result <- results[[celltype]]
+    result <- results[[celltype_N]]
+    # 檢查結果是否有效
+    if (is.null(result) || nrow(result) == 0) {
+      warning(paste("No valid", result_type, "results for", celltype_N))
+      return()
+    }
     # 繪製點狀圖
-    p = dotplot(result, showCategory = 10) + ggtitle(paste0(celltype, " ", result_type, " enrichment"))
+    p = dotplot(result, showCategory = 10) + ggtitle(paste0(celltype_N, " Negative ", result_type, " enrichment"))
     # 指定保存位置
-    filepath <- paste0("C:/Users/benson/Desktop/Research/Result_picture/GSE174188/", result_type, "/", celltype, "_", result_type, "_enrichment.png")
+    filepath <- paste0("C:/Users/benson/Desktop/Research/Result_picture/GSE174188/Pearson/Top200/", result_type, "/Negative/", celltype_N, "_negative_", result_type, "_enrichment.png")
     # 保存圖片
-    ggsave(filepath, plot = p)
-  })}
-
+    ggsave(filepath, plot = p)})}
 
 # 對 GO 結果進行繪圖
 plot_results(go_results, "GO")
-
 # 對 KEGG 結果進行繪圖
 plot_results(kegg_results, "KEGG")
+
+
+
+
+# Positive
+# 提取cell type列表
+celltype_P <- positive$X
+
+# 初始化一個空的列表來存儲結果
+genes <- list()
+
+# 對每個細胞類型進行操作
+genes <- lapply(celltype_P, function(x) {
+  temp <- positive[which(celltype_P == x), 2:ncol(positive[which(celltype_P == x),])]
+  temp <- temp %>% select_if(~!any(is.na(.))) # 刪除包含NA的列
+  temp <- unlist(temp) # 將temp轉換為向量
+  return(temp)})
+
+# 給列表的每個元素命名
+names(genes) <- celltype_P
+
+# 確認資料型態
+class(genes)
+
+# 對每種細胞類型的基因列表進行轉換
+ids <- lapply(genes, function(gene_list) {
+  bitr(gene_list, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)})
+
+# 進行GO(Gene Ontology) analysis
+go_results <- list()
+for(i in names(ids)){
+  go_results[[i]] <- enrichGO(gene         = ids[[i]]$ENTREZID,
+                              OrgDb         = org.Hs.eg.db,
+                              ont           = "BP",  # 進行 Biological Process 的分析
+                              pAdjustMethod = "BH",  # 使用 Benjamini & Hochberg (BH) 方法來調整 p 值
+                              pvalueCutoff  = 0.05,  # 設定 p 值的閾值為 0.05
+                              qvalueCutoff  = 0.05,  # 設定 q 值的閾值為 0.05
+                              readable      = TRUE)}  # 將 ENTREZ ID 轉換為可讀的基因名稱
+
+# 進行KEGG Enrichment analysis
+kegg_results <- list()
+for(i in names(ids)){
+  kegg_results[[i]] <- enrichKEGG(gene         = ids[[i]]$ENTREZID,
+                                  organism     = 'hsa',
+                                  pvalueCutoff = 0.05)}
+
+# 定義一個函數來繪製並保存結果
+plot_results <- function(results, result_type) {
+  lapply(names(results), function(celltype_P) {
+    # 提取結果
+    result <- results[[celltype_P]]
+    # 檢查結果是否有效
+    if (is.null(result) || nrow(result) == 0) {
+      warning(paste("No valid", result_type, "results for", celltype_N))
+      return()
+    }
+    # 繪製點狀圖
+    p = dotplot(result, showCategory = 10) + ggtitle(paste0(celltype_P, " Positive ", result_type, " enrichment"))
+    # 指定保存位置
+    filepath <- paste0("C:/Users/benson/Desktop/Research/Result_picture/GSE174188/Pearson/Top200/", result_type, "/Positive/", celltype_P, "_positive_", result_type, "_enrichment.png")
+    # 保存圖片
+    ggsave(filepath, plot = p)})}
+
+# 對 GO 結果進行繪圖
+plot_results(go_results, "GO")
+# 對 KEGG 結果進行繪圖
+plot_results(kegg_results, "KEGG")
+
+
+
 
